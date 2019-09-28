c23499a1d7cad78225279083aa927e1cb7e17e23
monsieurverde <domverdemgmt@gmail.com>
2019-09-27 21:42:00 -0400

    added a confirmation page

--- main.py
+++ main.py
@@ -5,10 +5,26 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 
-@app.route("/", methods=['GET', 'POST'])
-def index():
-    if request.method == 'GET':
-        return render_template('signup.html')
+@app.route('/')
+def user_signup():
+    return render_template('signup.html')
 
-    return redirect('confirmation.html',user_name=request.get['user-name'])
+
+
+@app.route("/", methods=['POST'])
+def user_response():
+
+    # THIS CREATES VARIABLES FROM THE FORM INPUTS
+
+    username = request.form['username']
+    #password = request.form['password']
+    #password_validate = request.form['password_validate']
+    #email = request.form['email']
+    username = username
+    return redirect('/welcome?username={0}'.format(username))
+
+@app.route("/welcome")
+def signup_complete():
+        username = request.args.get('username')
+        return render_template('confirmation.html',username=username)
 app.run()
